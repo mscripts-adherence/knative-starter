1f90d4ff5f8f3258d8a2780998892f2a857b01a0
Mark Dickson <mark.dickson02@cardinalhealth.com>
2022-01-05 12:26:53 -0500

    Outbound stuff added for cloud events

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const axios = require("axios").default;
+const { HTTP, CloudEvent } = require("cloudevents");
+
+const BROKER_URL = process.env.ADHERENCE_BROKER
+
+// TODO: switch to fastify from express
 
 let lastMessage = '';
 
@@ -13,16 +19,54 @@ app.get('/', (req, res) => {
   res.send(`Hello ${target}!\n`);
 });
 
+function sendCloudEvent(url, type, source, data) {
+  return new Promise((resolve, reject) => {
+    const message = HTTP.binary(new CloudEvent({type: type, source: source, data: data}))
+    axios({
+      method: "post",
+      url: url,
+      data: message.body,
+      headers: message.headers,
+    }).then((result) => {
+      resolve(result); // TODO: perhaps this checks the response code, etc, and rejects accordingly
+    }).catch(reject);
+  })
+}
+
 app.get('/inbound', (req, res) => {
-  lastMessage = JSON.stringify(req.body);
+  lastMessage = JSON.stringify(req.body) + " GET";
   const target = lastMessage || process.env.TARGET || "World";
-  res.send(`Hello ${target}!\n`);
+
+  // without a broker, just stop processing here and respond
+  if(!BROKER_URL) {
+    res.send(`Hello ${target}!\n`);
+    return;
+  }
+
+  // otherwise, send a cloud event
+  sendCloudEvent(BROKER_URL, "ack", "/knative-starter", {payload: req.body}).then((result) => {
+    res.send(`Hello ${target}!\n`);
+  }).catch((err) => {
+    res.status(500).send(`Hello ${target}!  I had an error ${err}\n`);
+  });
 });
 
 app.post('/inbound', (req, res) => {
-  lastMessage = JSON.stringify(req.body);
+  lastMessage = JSON.stringify(req.body) + " GET";
   const target = lastMessage || process.env.TARGET || "World";
-  res.send(`Hello ${target}!\n`);
+
+  // without a broker, just stop processing here and respond
+  if(!BROKER_URL) {
+    res.send(`Hello ${target}!\n`);
+    return;
+  }
+
+  // otherwise, send a cloud event
+  sendCloudEvent(BROKER_URL, "ack", "/knative-starter", {payload: req.body}).then((result) => {
+    res.send(`Hello ${target}!\n`);
+  }).catch((err) => {
+    res.status(500).send(`Hello ${target}!  I had an error ${err}\n`);
+  });
 });
 
 const port = process.env.PORT || 8080;
